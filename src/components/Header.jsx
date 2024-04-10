046ec99fa82cb25c1b0ce9b1544152421bb52d59
Brayan Esteves <besteves@moveapps.cl>
2024-04-10 17:48:26 -0400

    Structure Base[Layer: Components] - Header [10/04/2024]

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { Link, }    from 'react-router-dom';
+
+export const Header = () => {
+    return (
+        <header>        
+        <h1>Welcome to our website!</h1>
+        <nav>
+          <ul>
+            <li><Link to='/search-page'>Search Page</Link></li>
+            <li><Link to='/'>Home</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+};
