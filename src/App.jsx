9ee197544c2d71ae335e57808b84fa1eb53f9994
Brayan Esteves <besteves@moveapps.cl>
2024-04-10 17:51:51 -0400

    Structure Base - App.jsx (Implements "react-router-dom") [10/04/2024]

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import './App.css';
+import                            './App.css';
+import { Route, Routes, }    from 'react-router-dom';
+import { Header }            from './components/Header';
+
+// <COMPONENT> //
+const Home       = () => <h1>Home</h1>;
+const SearchPage = () => <h1>Search Page</h1>;
+// <.COMPONENT> //
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header />
+      <Routes>        
+        <Route path='/' element={<Home />} />
+        <Route path='/search-page' element={<SearchPage />} />      
+      </Routes>
+    </div>
   )
 }
 
