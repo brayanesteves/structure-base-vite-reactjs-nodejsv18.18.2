075508fe0d8f2261e7f197b551b59bb6deaf7df2
Brayan Esteves <besteves@moveapps.cl>
2024-04-11 15:16:25 -0400

    Structure Base[Layer: Components] - Custom "Header" -> "NavLinks" [11/04/2024]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,12 @@
-import { Link, } from 'react-router-dom';
+import { Link, NavLink as NavLinkCustom } from 'react-router-dom';
+
+const NavLink = ({ to, children, ...props }) => {
+    return (
+        <NavLinkCustom {...props} className={({isActive}) => {
+            return isActive ? 'is-active' : '';
+        }} to={to}>{children}</NavLinkCustom>
+    );
+};
 
 export const Header = () => {
     return (
@@ -6,8 +14,10 @@ export const Header = () => {
         <h1>Welcome to our website!</h1>
         <nav>
           <ul>
-            <li><Link to='/search-page'>Search Page</Link></li>
-            <li><Link to='/'>Home</Link></li>
+            <li><NavLink className={({isActive}) => {
+                return isActive ? 'is-active' : '';
+            }} to='/'>Home</NavLink></li>
+            <li><NavLink to='/search-page'>Search Page</NavLink></li>
           </ul>
         </nav>
       </header>
